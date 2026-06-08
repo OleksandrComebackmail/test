f6080c8c5d6d0fc41fb288da2ec1fc74979eeaf2
OleksandrComebackmail <Alexander.k@softdoes.co>
2026-06-08 22:25:16 +0300

    feat: add clamp helper

--- src/index.js
+++ src/index.js
@@ -10,3 +10,5 @@ export const round = (n, decimals = 0) =>
   Math.round(n * 10 ** decimals) / 10 ** decimals;
 
 export const average = (xs) => xs.reduce((a, b) => a + b, 0) / xs.length;
+
+export const clamp = (n, min, max) => Math.min(Math.max(n, min), max);
